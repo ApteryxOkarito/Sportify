81db4f8e6e0d0adf269ee09833cc840475a1d87d
JLLD47 <oormzoo@gmail.com>
2024-02-27 23:00:49 +0000

    cleaner code

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const { checkDB, syncModels } = require("./db/index.js")
 const { addRelations } = require('./db/relations.js')
-const User = require('./api/models/user.model.js')
-const Suscription = require('./api/models/suscription.model.js')
-const Class = require('./api/models/class.model.js')
-const Sport = require('./api/models/sport.model.js')
-const Room = require('./api/models/room.model.js')
-const Material = require('./api/models/material.model.js')
 
 
 async function dbConnect() {
