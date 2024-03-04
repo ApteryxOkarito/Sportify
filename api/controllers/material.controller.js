4da72e515f83b5c6d6cc3a0c3dbd8c463f4d3aa9
JLLD47 <oormzoo@gmail.com>
2024-03-04 17:48:32 +0000

    Función updateMaterial añadida

--- api/controllers/material.controller.js
+++ api/controllers/material.controller.js
@@ -78,12 +78,22 @@ const createMaterial = async (req,res) => {
 const updateMaterial = async (req,res) => {
     try {
 
-      
+        const updatedRows = await Material.update(req.body,{
+            where:{
+                id: req.params.materialId
+            }
+        })
+
+        if(updatedRows > 0) {
+            const updatedMaterial = await Material.findByPk(req.params.materialId)
+            return res.status(200).json(updatedMaterial)
+          } else {
+            return res.status(404).send('Material not found')
+          }
         } catch (error) {
           res.status(500).send(error.message)
         }
       }
-
 
 const deleteMaterial = async (req,res) => {
     try {
