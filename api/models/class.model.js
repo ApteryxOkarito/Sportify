771ad7f6b6273271920b898107645316a524ffd0
ApteryxOkarito <pelayo.violeta@gmail.com>
2024-03-05 20:14:15 +0100

    Cambiado el tipo de dato duracion de classe de varchar a un integer

--- api/models/class.model.js
+++ api/models/class.model.js
@@ -12,7 +12,7 @@ const Class = sequelize.define('class', {
         allowNull: false
     },
     duration: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
         allowNull: false
 
     },
